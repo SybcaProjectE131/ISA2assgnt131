27709ef7669a769e715471e8563c2a2f6ad8827d
Eymard <Eymard@DESKTOP-D911G30>
2024-03-28 10:15:57 +0530

    commit

---
+++ src/question6/PersonTest.java
@@ -0,0 +1,37 @@
+package question6;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PersonTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+    public void testSetNameAndGetAge() {
+        Person person = new Person("John", 30);
+        person.setName("Alice");
+        assertEquals("Alice", person.getName());
+        assertEquals(30, person.getAge());
+    }
+
+}
