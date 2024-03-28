ec156d0c1e12c052c20ed676807054b520af08b3
Eymard <Eymard@DESKTOP-D911G30>
2024-03-28 09:58:57 +0530

    commit

---
+++ src/question2/Factorial.java
@@ -0,0 +1,27 @@
+package question2;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan =new Scanner(System.in);
+		
+		int num;
+		int fact=1;
+		System.out.println("Enter a number:");
+		num=scan.nextInt();
+		
+		for(int i=1;i<=num;i++) {
+			
+			fact=fact*i;
+			
+		}
+		
+		System.out.println("Factorial of "+num+" is:"+fact);
+		
+	}
+
+}
